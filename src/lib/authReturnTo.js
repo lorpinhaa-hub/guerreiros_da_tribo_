4ccf15f5d8e089bc325062de4b0ca04bb5910ab2
lorpinhaa-hub <lorpinhaa@gmail.com>
2026-08-19 09:00:16 -0300

    Update authReturnTo.js

--- src/lib/authReturnTo.js
+++ src/lib/authReturnTo.js
@@ -1,4 +1,4 @@
-export function authReturnTo() {
+export function safeReturnTo() {
   const params = new URLSearchParams(window.location.search);
   const retorno = params.get("returnTo");
   return retorno || "/inicio";
